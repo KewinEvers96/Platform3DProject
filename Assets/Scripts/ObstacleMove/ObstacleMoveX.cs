c26242a9cbdda0897498fd12e2614700b4b0549d
variosva07 <variosva@unal.edu.co>
2022-01-19 22:52:45 -0500

    se agregan obstaculos

---
+++ Assets/Scripts/ObstacleMove/ObstacleMoveX.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleMoveX : MonoBehaviour
+{
+
+    private bool MoverRight = true;
+
+    [SerializeField]
+    float move =2;
+    [SerializeField]
+    float LimiteDerecho = 4;
+    [SerializeField]
+    float LimiteIzquierdo = -4;
+
+
+    void Update()
+    { 
+        transform.Translate(move*Time.deltaTime,0,0);
+        if (transform.position.x>=LimiteDerecho && MoverRight)
+        {
+            move=move*(-1);
+            MoverRight=false;
+         
+        }else if (transform.position.x<=LimiteIzquierdo && MoverRight==false )
+        {
+            move=move*(-1);
+            MoverRight=true;
+        }
+    }
+
+
+}
