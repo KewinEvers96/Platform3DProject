5ec28f50d0a6076a523b4c0bf03fdb70a22c4849
juczapatama <juczapatama@unal.edu.co>
2022-02-04 01:27:54 -0500

    menu principal terminado

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void PlayLevel1(){
+        SceneManager.LoadScene(1);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void PlayLevel2(){
+        SceneManager.LoadScene(2);
+    }
+    public void Creditos(){
+        SceneManager.LoadScene("Level2");
+    }
+    public void Regresar(){
+        SceneManager.LoadScene("Level2");
+    }
+    public void Salir(){
+        Application.Quit();
     }
 }
