1ac2e6b72867e94e35e8ce0ca2ee4ed88e5dd21a
juczapatama <juczapatama@unal.edu.co>
2022-02-04 18:42:19 -0500

    menus finalizados

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -11,11 +11,9 @@ public class MainMenu : MonoBehaviour
     public void PlayLevel2(){
         SceneManager.LoadScene(2);
     }
-    public void Creditos(){
-        SceneManager.LoadScene("Level2");
-    }
+    
     public void Regresar(){
-        SceneManager.LoadScene("Level2");
+        SceneManager.LoadScene(0);
     }
     public void Salir(){
         Application.Quit();
